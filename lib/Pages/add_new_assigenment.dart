1e05af468cf97e3904ca5a695e07639b66305e82
Sanidu Dinethma <sanidusd.lk@gmail.com>
2026-06-15 00:56:13 +0530

    fxed error

--- lib/Pages/add_new_assigenment.dart
+++ lib/Pages/add_new_assigenment.dart
@@ -3,39 +3,31 @@ import 'package:study_planner/consts/Styles.dart';
 import 'package:study_planner/models/courses_model.dart';
 import 'package:study_planner/widget/custom_text_field.dart';
 
-class AddNewAssigenment extends StatefulWidget {
+class AddNewAssigenment extends StatelessWidget {
   final Courses course;
   const AddNewAssigenment({super.key, required this.course});
 
   @override
-  State<AddNewAssigenment> createState() => _AddNewAssigenmentState();
-}
-
-class _AddNewAssigenmentState extends State<AddNewAssigenment> {
-  TextEditingController _textEditingController = TextEditingController();
-  @override
   Widget build(BuildContext context) {
+    final TextEditingController _titleController = TextEditingController();
+    final TextEditingController _descriptionController =
+        TextEditingController();
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Add New Assignment", style: Styles.pageHeading),
-            SizedBox(height: 10),
-            Text(
-              "Fill in the details below to add a new assignment. And start managing your study planner.",
-              style: Styles.bodyText,
-            ),
-            SizedBox(height: 10),
-            CustomTextField(
-              controller: _textEditingController,
-              label: 'Name',
-              keyboardType: TextInputType.name,
-            ),
-          ],
-        ),
+      body: Column(
+        children: [
+          Text("Add new assignment", style: Styles.pageHeading),
+          SizedBox(height: 20),
+          Text(
+            "Fill in the details below to add a new assignment. And start managing your study planner.",
+          ),
+          SizedBox(height: 20),
+          CustomTextField(
+            controller: _titleController,
+            keyboardType: TextInputType.numberWithOptions(decimal: true),
+            label: "Assignment Title",
+          ),
+        ],
       ),
     );
   }
--- lib/Pages/add_new_notes.dart
+++ lib/Pages/add_new_notes.dart
@@ -1,17 +1,1 @@
-import 'package:flutter/material.dart';
-import 'package:study_planner/models/courses_model.dart';
 
-class AddNewNotes extends StatefulWidget {
-  final Courses course;
-  const AddNewNotes({super.key, required this.course});
-
-  @override
-  State<AddNewNotes> createState() => _AddNewNotesState();
-}
-
-class _AddNewNotesState extends State<AddNewNotes> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar());
-  }
-}
